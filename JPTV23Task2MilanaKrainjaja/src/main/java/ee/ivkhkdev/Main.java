ae1a2c44da34d0278f460008e111eaf21cf73560
pupil <pupil@ivkhk.ee>
2024-09-23 09:29:20 +0300

    first commit

---
+++ JPTV23Task2MilanaKrainjaja/src/main/java/ee/ivkhkdev/Main.java
@@ -0,0 +1,19 @@
+package ee.ivkhkdev;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите трёхзначное число: ");
+        int number = scanner.nextInt();
+        int u = number % 10;
+        int t = (number / 10) % 10;
+        int h = number / 100;
+        int sumofdigits = h + t + u;
+        System.out.println("Число единиц: " + u);
+        System.out.println("Число десятков: " + t);
+        System.out.println("Сумма цифр: " + sumofdigits);
+    }
+}
